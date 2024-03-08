4941ea0a3025ad474d3d4083622643d2c306de57
Marco-parra <162514354+Marco-parra@users.noreply.github.com>
2024-03-08 13:16:25 -0700

    Add files via upload

---
+++ Ejercicio-js/style/scritp/ejercicio.js
@@ -0,0 +1,11 @@
+function cambiarcolor(){  
+    variable_album=document.getElementById("album");
+    variable_album.style.backgroundColor = "red";
+}
+
+function cambiartamano(){
+    const imagen = document.getElementsByClassName('imagen')
+    const imagen1 = imagen[0];
+    imagen1.style.width="100px";
+    imagen1.style.height="100";
+}
